bfbcc69ec7c3d62e5f3d7b1ff11ab63d9dc9e778
jurelik <jure@likar.me>
2018-12-17 21:46:21 -0800

    trying to get the mp3 to play

--- main.js
+++ main.js
@@ -1,1 +1,19 @@
-console.log('sup');
+const AudioContext = window.AudioContext || window.webkitAudioContext;
+const context = new AudioContext();
+
+let buffer;
+
+let req = new XMLHttpRequest();
+req.onload = function () {
+  let source = context.createBufferSource();
+  context.decodeAudioData(req.response, decoded => {
+    buffer = decoded;
+    source.buffer = decoded;
+    source.start();
+    console.log(buffer);
+  });
+}
+req.open('GET', 'warble.mp3', true);
+req.responseType = 'arraybuffer';
+req.send();
+
