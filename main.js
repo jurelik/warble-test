e68dbeee3a0ba924cbfbe7715e72f7c61849345b
jurelik <jure@likar.me>
2018-12-20 19:41:26 -0800

    implemented a working square warble

--- main.js
+++ main.js
@@ -3,17 +3,37 @@ const context = new AudioContext();
 
 let buffer;
 
-let req = new XMLHttpRequest();
-req.onload = function () {
-  let source = context.createBufferSource();
-  context.decodeAudioData(req.response, decoded => {
-    buffer = decoded;
-    source.buffer = decoded;
-    source.start();
-    console.log(buffer);
-  });
+function play() {
+  let toggle = false;
+  let req = new XMLHttpRequest();
+
+  req.onload = function () {
+    function warble() {
+      setTimeout(() => {
+        if (toggle === false) {
+            source.playbackRate.value = 0.98;
+          toggle = true;
+          warble();
+        }
+        else {
+          source.playbackRate.value = 1;
+          toggle = false;
+          warble();
+        }
+      }, 10);
+    };
+    let source = context.createBufferSource();
+    context.decodeAudioData(req.response, decoded => {
+      buffer = decoded;
+      source.buffer = decoded;
+      source.connect(context.destination);
+      source.playbackRate.value = 0.9;
+      source.start();
+      warble();
+    });
+  }
+  req.open('GET', 'warble.mp3', true);
+  req.responseType = 'arraybuffer';
+  req.send();
 }
-req.open('GET', 'warble.mp3', true);
-req.responseType = 'arraybuffer';
-req.send();
 
